bf5cb2b2abf809af83130c4ccd6cafefbf0f3d0e
Roman Shamov <dreigfire@mail.ru>
2022-12-12 13:34:39 +0300

    Version 1.3.1
    +Fixed dependencies in the plugin.yml file.
    Before was
    depend:ProtocolLib, ProjectKorra, Spirits, JedCore
    now it is
    depend: ProtocolLib, ProjectKorra
    softdepend: Spirits, Hyperion, JedCore
    
    +Fixed comments generation I guess. In 1.3 it worked only with /dspkreload
    
    +Changed groupId from "me.dreig_michihi" to "me.dreig_michihi.damagesplashespk"
    +ArtifactId changed to lowercase
    +Version changed to 1.3.1

--- src/main/java/me/dreig_michihi/damagesplashespk/config/comments/ConfigComments.java
+++ src/main/java/me/dreig_michihi/damagesplashespk/config/comments/ConfigComments.java
@@ -1,10 +1,9 @@
 package me.dreig_michihi.damagesplashespk.config.comments;
 
-import com.projectkorra.projectkorra.ProjectKorra;
 import me.dreig_michihi.damagesplashespk.DamageSplashesPK;
 import me.dreig_michihi.damagesplashespk.config.SplashesConfig;
+import org.bukkit.Bukkit;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -15,43 +14,45 @@ import java.util.ListIterator;
 public class ConfigComments {
 
     public static void addDefaultComment(String configPath, String comment, boolean inline) {
-        try {
-            Path path = SplashesConfig.getFile().toPath();
-            List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
-            if (!inline)
-                lines.add(0, "");
-            ListIterator<String> configPathSeparated = List.of(configPath.split("\\.")).listIterator();
-            ListIterator<String> iter = lines.listIterator();
-            String stringFinder = configPathSeparated.next();
-            while (iter.hasNext()) {
-                String curr = iter.next();
-                if (curr.trim().startsWith(stringFinder)) {
-                    if (configPathSeparated.hasNext())
-                        stringFinder = configPathSeparated.next();
-                    else {
-                        if (inline) {
-                            if (!curr.contains("#")) {
-                                iter.set(curr + " # " + comment);
+        Bukkit.getScheduler().runTask(DamageSplashesPK.plugin, ()->{
+            try {
+                Path path = SplashesConfig.getFile().toPath();
+                List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+                if (!inline)
+                    lines.add(0, "");
+                ListIterator<String> configPathSeparated = List.of(configPath.split("\\.")).listIterator();
+                ListIterator<String> iter = lines.listIterator();
+                String stringFinder = configPathSeparated.next();
+                while (iter.hasNext()) {
+                    String curr = iter.next();
+                    if (curr.trim().startsWith(stringFinder)) {
+                        if (configPathSeparated.hasNext())
+                            stringFinder = configPathSeparated.next();
+                        else {
+                            if (inline) {
+                                if (!curr.contains("#")) {
+                                    iter.set(curr + " # " + comment);
+                                }
+                            } else {
+                                iter.previous();
+                                String prev = iter.previous();
+                                iter.next();
+                                if (!prev.trim().startsWith("#")) {
+                                    iter.add("# " + comment);
+                                }
+                                iter.next();
                             }
-                        } else {
-                            iter.previous();
-                            String prev = iter.previous();
-                            iter.next();
-                            if (!prev.trim().startsWith("#")) {
-                                iter.add("# " + comment);
-                            }
-                            iter.next();
+                            break;
                         }
-                        break;
                     }
                 }
+                if (!inline)
+                    lines.remove(0);
+                Files.write(path, lines, StandardCharsets.UTF_8);
+            } catch (IOException e) {
+                DamageSplashesPK.plugin.getLogger().info("COMMENTS ERROR");
+                //throw new RuntimeException(e);
             }
-            if (!inline)
-                lines.remove(0);
-            Files.write(path, lines, StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            DamageSplashesPK.plugin.getLogger().info("COMMENTS ERROR");
-            //throw new RuntimeException(e);
-        }
+        });
     }
 }
